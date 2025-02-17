306761e4223423db602f8ce681ffed6d737cf683
juba0713 <160235657+juba0713@users.noreply.github.com>
2025-02-17 08:52:19 +0800

    Update HomeController.java

--- ProRequest/src/main/java/com/prorequest/controllers/HomeController.java
+++ ProRequest/src/main/java/com/prorequest/controllers/HomeController.java
@@ -3,7 +3,7 @@ package com.prorequest.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
+//Test
 @Controller
 @RequestMapping("/pro-request")
 public class HomeController {
